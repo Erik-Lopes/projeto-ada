b17089c3c830b4e902dbc8fcc086400b8b50a715
Erik <eriklopes562@gmail.com>
2025-09-12 11:49:06 -0300

    Edição de modal, e simulação de comportamento para a API

--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,6 @@
+document.getElementById("modal-senha").style.display = "none";
+document.getElementById("modal-cadastro").style.display = "none";
+
 function abrirModalCadastro(){
   const btnCadastrar = document.getElementById("btnCadastrar");
   const span = document.querySelector(".cadastroClose");
@@ -13,5 +16,23 @@ function abrirModalCadastro(){
     }
   }
 }
+
+function abrirModalSenha(){
+  const btnSenha = document.getElementById("btnSenha");
+  const span = document.querySelector(".senhaClose");
+  const modal = document.getElementById("modal-senha");
+
+  btnSenha.onclick = () => modal.style.display = "block";
+
+  span.onclick = () => modal.style.display = "none";
+
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+abrirModalSenha();
 
 abrirModalCadastro();
