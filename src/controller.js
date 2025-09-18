2bc161ea478db722ff42216b8355de28eb3de87c
Erik <eriklopes562@gmail.com>
2025-09-18 12:03:43 -0300

    Integração com API - Funcionalidades: cadastro e login

--- src/controller.js
+++ src/controller.js
@@ -2,11 +2,13 @@ document.getElementById("modal-senha").style.display = "none";
 document.getElementById("modal-cadastro").style.display = "none";
 
 function abrirModalCadastro(){
-  const btnCadastrar = document.getElementById("btnCadastrar");
+  // const btnCadastrar = document.getElementById("btnCadastrar");
   const span = document.querySelector(".cadastroClose");
   const modal = document.getElementById("modal-cadastro");
+  document.getElementById("email-cadastro").value = document.getElementById("email").value;
 
-  btnCadastrar.onclick = () => modal.style.display = "block";
+  // btnCadastrar.onclick = () => 
+  modal.style.display = "block";
 
   span.onclick = () => modal.style.display = "none";
 
@@ -18,11 +20,12 @@ function abrirModalCadastro(){
 }
 
 function abrirModalSenha(){
-  const btnSenha = document.getElementById("btnSenha");
+  // const btnSenha = document.getElementById("btnSenha");
   const span = document.querySelector(".senhaClose");
   const modal = document.getElementById("modal-senha");
 
-  btnSenha.onclick = () => modal.style.display = "block";
+  // btnSenha.onclick = () => 
+  modal.style.display = "block";
 
   span.onclick = () => modal.style.display = "none";
 
@@ -38,7 +41,7 @@ function submitCadastro(){
   
   const form = document.getElementById("formulario-cadastro");
   
-  form.addEventListener("submit", function(event){
+  form.addEventListener("submit", async function(event){
     event.preventDefault();
     const objeto = {
       email: document.getElementById("email-cadastro").value,
@@ -51,10 +54,68 @@ function submitCadastro(){
 
     const objetoJson = JSON.stringify(objeto);
     console.log(objetoJson);
+
+    const res = await fetch("http://localhost:3000/api/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: objetoJson
+    });
+    console.log(res);
+    if(res.ok){
+      alert("Usuario cadastrado com sucesso!");
+    }
   });
 }
 
+//Função para formatar os dados em JSON
+function submitSenha(){
+  
+  const form = document.getElementById("form-senha");
+  
+  form.addEventListener("submit", async function(event){
+    event.preventDefault();
+    const objeto = {
+      email: document.getElementById("email").value,
+      senha: document.getElementById("senha").value,
+    }
 
+    const objetoJson = JSON.stringify(objeto);
+    console.log(objetoJson);
+
+    const res = await fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: objetoJson
+    });
+    console.log(res);
+    if(res.ok){
+      alert("Usuario logado com sucesso!");
+    }
+  });
+}
+
+function validaEmail (){
+  const form = document.getElementById("insere-email");
+  
+  form.addEventListener("submit", async function(event){
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const res = await fetch(`http://localhost:3000/api/users/validate-email?email=${email}`);
+
+    console.log(res)
+    if(res.status === 409){
+      abrirModalSenha();
+    }else{
+      abrirModalCadastro();
+    }
+  
+  });
+}
+
+validaEmail();
 submitCadastro();
-abrirModalSenha();
-abrirModalCadastro();
+submitSenha();
+
+// submitCadastro();
+// abrirModalSenha();
+// abrirModalCadastro();
