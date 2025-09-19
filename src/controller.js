2f5c830e5923447da552173720b8b4ae352c3ddb
Erik <eriklopes562@gmail.com>
2025-09-18 23:42:21 -0300

    Funcionalidades prontas

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,6 @@
 document.getElementById("modal-senha").style.display = "none";
 document.getElementById("modal-cadastro").style.display = "none";
+document.getElementById("modal-dados").style.display = "none";
 
 
 function abrirModalCadastro(){
@@ -7,6 +8,23 @@ function abrirModalCadastro(){
   const span = document.querySelector(".cadastroClose");
   const modal = document.getElementById("modal-cadastro");
   document.getElementById("email-cadastro").value = document.getElementById("email").value;
+
+  // btnCadastrar.onclick = () => 
+  modal.style.display = "block";
+
+  span.onclick = () => modal.style.display = "none";
+
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+function abrirModalDados(){
+  // const btnCadastrar = document.getElementById("btnCadastrar");
+  const span = document.querySelector(".dadosClose");
+  const modal = document.getElementById("modal-dados");
 
   // btnCadastrar.onclick = () => 
   modal.style.display = "block";
@@ -92,7 +110,6 @@ function submitSenha(){
       method: "POST",
       headers: { "Content-Type": "application/json",
         "Authorization": 'Bearer ${email}'
-
       },
       body: objetoJson
     });
@@ -104,6 +121,8 @@ function submitSenha(){
       retornaDados();
       fechaModalSenha();
       document.getElementById("email").value = "";
+      document.getElementById("senha").value = "";
+      abrirModalDados();
     }
     else{
       alert("Senha inválida! Tente novamente")
@@ -128,28 +147,101 @@ function validaEmail (){
   });
 }
 
+async function retornaDados(){
+  
+  const btnExcluir = document.getElementById("botao-excluir-dados");
+  const btnSalvar = document.getElementById("botao-salvar-dados");
+  const btnEditar = document.getElementById("botao-editar-dados");
+  const inputs = document.querySelectorAll("#formulario-dados input")
+
+  const res = await fetch("http://localhost:3000/api/users", {
+    method: "GET",
+    headers: { "Content-Type": "application/json", 
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    },
+  });
+    
+  const dados = JSON.parse(JSON.stringify(await res.json()))
+
+  document.getElementById("nome-dados").value = dados.nome;
+  document.getElementById("email-dados").value = dados.email;
+  document.getElementById("telefone-dados").value = dados.telefone;
+  document.getElementById("endereco-dados").value = dados.endereco;
+  document.getElementById("cpf-dados").value = dados.cpf;
+
+  btnEditar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    if(btnEditar.textContent == "Editar"){
+      btnEditar.textContent = "Cancelar";
+      btnSalvar.style.display = "block";
+      btnExcluir.style.display = "none";
+      inputs.forEach(input => {
+        input.readOnly = !input.readOnly;
+      });
+    }else{
+      btnEditar.textContent = "Editar";
+      btnSalvar.style.display = "none";
+      btnExcluir.style.display = "block";
+      inputs.forEach(input => {
+        input.readOnly = !input.readOnly;
+      });
+      document.getElementById("nome-dados").value = dados.nome;
+      document.getElementById("email-dados").value = dados.email;
+      document.getElementById("telefone-dados").value = dados.telefone;
+      document.getElementById("endereco-dados").value = dados.endereco;
+      document.getElementById("cpf-dados").value = dados.cpf;
+    }
+  });
+
+  btnExcluir.addEventListener("click", function(event){
+    event.preventDefault();
+    excluirUsuario(dados.id)
+  });
+
+  btnSalvar.addEventListener("click", function(event){
+    event.preventDefault();
+    editarUsuario(dados.id)
+  });
+}
+
+async function excluirUsuario(id){
+  
+  const res = await fetch(`http://localhost:3000/api/users/${id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json", 
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    },
+  });
+  if(res.ok){
+    alert("Usuário excluido com sucesso!")
+    localStorage.removeItem('token');
+  }
+}
+
+async function editarUsuario(id){
+  
+  const objeto = {
+      email: document.getElementById("email-dados").value,
+      nome: document.getElementById("nome-dados").value,
+      telefone: document.getElementById("telefone-dados").value,
+      endereco: document.getElementById("endereco-dados").value,
+      cpf: document.getElementById("cpf-dados").value,
+    }
+  const objetoJson = JSON.stringify(objeto);
+  const res = await fetch(`http://localhost:3000/api/users/${id}`, {
+    method: "PUT",
+    body: objetoJson,
+    headers: { "Content-Type": "application/json", 
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    },
+  });
+  if(res.ok){
+    alert("Dados alterados com sucesso!")
+    document.getElementById("modal-dados").style.display = "none";
+  }
+}
 
 validaEmail();
 submitCadastro();
 submitSenha();
-
-
-// submitCadastro();
-// abrirModalSenha();
-// abrirModalCadastro();
-
-async function retornaDados(){
-  
-    const res = await fetch("http://localhost:3000/api/users", {
-      method: "GET",
-      headers: { "Content-Type": "application/json", 
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      },
-    });
-    
-    const dados = JSON.parse(JSON.stringify(await res.json()))
-
-    
-
-}
-
